a9c22f64169b257b83c5e910b3a17dad19297849
manisha patel <manisha.patel.121423@gmail.com>
2024-03-29 15:43:06 -0400

    updated css to floal left right also added tips what recipes to search for

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -4,10 +4,21 @@ import { useState } from 'react';
 
 export default function HeaderNav({value , onChange}){
 
-    return <div className={styles.header}>Recipe App
-        <label className={styles.checkboxRight} >
-          <input type="checkbox" checked={value} onChange={onChange}  />
-          Load Sample Data From File
-        </label>
-    </div>
+    return (
+    <>
+        <div className={styles.headerBox}>
+            <div className={styles.headerLeft}>
+                    Recipe App
+            </div>
+            <div className={styles.headerRight}>
+                <label className={styles.checkboxRight} >
+                    <input type="checkbox" checked={value} onChange={onChange}  />
+                    Load Sample Data From File
+                </label>
+            </div>
+
+        </div>
+   
+    </>
+    )
 }
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,9 +3,16 @@ import styles from './searchbox.module.css';
 
 const SearchBox = ({ value, onChange }) => {
   return (
-    <div className={styles.searchContainer}>
+    <>
+    <div className={styles.parentSearch}>
+      <div className={styles.searchboxLeft}>
         <input className={styles.input} type="text" value={value} onChange={onChange} placeholder='Search' />
-    </div>
+      </div>
+      <div className={styles.tipsRight}>
+      ( e.g pasta, pizza, cake, cocktail )
+      </div>
+   </div>
+    </>
   );
 };
 
